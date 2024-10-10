9b4250afd20bfba4130ab9a60e4d1e6b7bff9b26
Martin Häggblom <martin.haggblom@gmail.com>
2024-10-10 15:50:55 +0300

    submit 4.2 Blog List, step 2

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+const blogsRouter = require('./controllers/blogs');
+
+//MongoDB connection via .env
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('MongoDB connection success!');
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB:', error.message);
+  });
+
+//middleware for allowing cross-origin-requests
+app.use(cors());
+//middleware for parsing request body
+app.use(express.json());
+
+//routes
+app.use('/api/blogs', blogsRouter);
+
+//error handling middleware (placeholder)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
